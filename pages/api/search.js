ebca5dbf663a20dc4559fb3709d61d9adcba0caa
Julian <julian.costinea@gmail.com>
2022-04-21 17:06:15 +0200

    modifying local api calls to live

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,20 +1,7 @@
 import { connectToDatabase } from "../../lib/mongodb";
-const sanitize = require("mongo-sanitize");
 
 async function handler(req, res) {
-    try {
-      const collection = "videogames";
-      const { db } = await connectToDatabase();
-      const dbCollection = db.collection(collection);
-      const result = await dbCollection
-        .find({ title: "Persona 5" })
-        .toArray();
-      res.status(200).json({ message: "success", result: result });
-    } catch (error) {
-      res.status(error.code ?? 502).send({
-        message: error.message ?? "Could not find any item",
-      });
-    }
+  res.status(200).json({ name: 'John Doe' })
 }
 
 export default handler;
