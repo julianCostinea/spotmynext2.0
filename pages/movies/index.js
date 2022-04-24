d217eeb9e38e43ebd9a7ff3cf1decde2d04080ef
Julian <julian.costinea@gmail.com>
2022-04-24 15:24:00 +0200

    added imagePath for all items

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -7,21 +7,21 @@ import FrontImage from "../../components/FrontImage/FrontImage";
 import classes from './movies.module.css';
 
 const Movies = (props) => {
-  // const { popularItems } = props;
-  // const fetchedPopularRecommendations = popularItems.result.map(
-  //   (item, index) => (
-  //     <Recommendation
-  //       key={item._id}
-  //       id={item._id}
-  //       title={item.title}
-  //       description={item.description}
-  //       photo={item.photo}
-  //       mainTags={item.mainTags}
-  //       secondaryTags={item.secondaryTags}
-  //       recommendations={item.recommendations}
-  //     />
-  //   )
-  // );
+  const { popularItems } = props;
+  const fetchedPopularRecommendations = popularItems.result.map(
+    (item, index) => (
+      <Recommendation
+        key={item._id}
+        id={item._id}
+        title={item.title}
+        description={item.description}
+        photo={item.photo}
+        mainTags={item.mainTags}
+        secondaryTags={item.secondaryTags}
+        recommendations={item.recommendations}
+      />
+    )
+  );
 
   return (
     <React.Fragment>
@@ -31,23 +31,23 @@ const Movies = (props) => {
         placeholder="Star Wars, Avengers, Goodfellas"
       />
       <h1 className={classes.hotPicksHeader}>Hot picks: </h1>
-      {/* <Recommendations>{fetchedPopularRecommendations}</Recommendations> */}
+      <Recommendations>{fetchedPopularRecommendations}</Recommendations>
     </React.Fragment>
   );
 };
 
-// export async function getStaticProps() {
-//   const res = await fetch(
-//     "https://spotmynext2-0.vercel.app/api/popularItems?collection=movies"
-//   );
-//   const popularItems = await res.json();
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://spotmynext2-0.vercel.app/api/popularItems?collection=movies"
+  );
+  const popularItems = await res.json();
 
-//   return {
-//     props: {
-//       popularItems,
-//     },
-//     revalidate: 3600,
-//   };
-// }
+  return {
+    props: {
+      popularItems,
+    },
+    revalidate: 3600,
+  };
+}
 
 export default Movies;
